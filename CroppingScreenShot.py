9324c22d174c1d39876403ed7c8d6e06c857e126
Sameer Butt <bsameer364@gmail.com>
2025-04-15 18:06:42 +0000

    Updated CroppingScreenShot.py (Made it better)

--- CroppingScreenShot.py
+++ CroppingScreenShot.py
@@ -12,7 +12,7 @@ if not os.path.exists(output_folder):
 
 # Define crop box (left, upper, right, lower)
 # Adjust these values to match the area where the emoji is located on screen
-crop_box = (1600, 820, 1680, 900) # Example values — you MUST tweak this based on your screen
+crop_box = (1603, 827, 1667, 891) # Example values — you MUST tweak this based on your screen
 
 # Process each labeled screenshot
 with open(label_file_path, 'r', encoding='utf-8') as f:
